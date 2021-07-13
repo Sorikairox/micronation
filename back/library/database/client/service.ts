064ecc0b6c8f9fefa0cc28df6fc3a9e110ad0394
Sorikairo <sorikairox@gmail.com>
2021-07-13 21:32:56 +0200

    fix(database client): lint

--- back/library/database/client/service.ts
+++ back/library/database/client/service.ts
@@ -3,17 +3,17 @@ import { MongoClient } from "mongodb";
 
 @Injectable()
 export class DatabaseClientService implements OnModuleInit {
-    public client: MongoClient;
-    public connectionPromise;
-    constructor(@Inject('CONFIG_OPTIONS') private options) {
-        this.client = new MongoClient(options.uri, {useUnifiedTopology: true});
-    }
+  public client: MongoClient;
+  public connectionPromise;
+  constructor(@Inject("CONFIG_OPTIONS") private options) {
+    this.client = new MongoClient(options.uri, { useUnifiedTopology: true });
+  }
 
-    async onModuleInit(): Promise<void> {
-        this.client = await this.client.connect();
-    }
+  async onModuleInit(): Promise<void> {
+    this.client = await this.client.connect();
+  }
 
-    getDb() {
-        return this.client.db(this.options.dbName);
-    }
+  getDb() {
+    return this.client.db(this.options.dbName);
+  }
 }
