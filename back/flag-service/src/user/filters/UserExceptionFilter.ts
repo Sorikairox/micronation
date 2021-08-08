018b8fe16e146fd2154965b48f84f8f9194102dc
Alice Gaudon <alice@gaudon.pro>
2021-08-08 10:50:52 +0200

    fix(flag/user-module): add @Catch to filter caught exceptions in UserExceptionFilter

--- back/flag-service/src/user/filters/UserExceptionFilter.ts
+++ back/flag-service/src/user/filters/UserExceptionFilter.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, ExceptionFilter } from '@nestjs/common';
+import { BadRequestException, Catch, ExceptionFilter } from '@nestjs/common';
 import { EmailAlreadyTakenError } from '../errors/EmailAlreadyTakenError';
 import { EmailNotFoundError } from '../errors/EmailNotFoundError';
 import { IncorrectPasswordError } from '../errors/IncorrectPasswordError';
 import { NicknameAlreadyTakenError } from '../errors/NicknameAlreadyTakenError';
 
+@Catch(EmailAlreadyTakenError, NicknameAlreadyTakenError, EmailNotFoundError, IncorrectPasswordError)
 export class UserExceptionFilter implements ExceptionFilter {
     catch(e: Error) {
         if (e instanceof EmailAlreadyTakenError || e instanceof NicknameAlreadyTakenError ||
