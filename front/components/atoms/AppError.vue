b6131d799b36cbc256f2ae1e55de87cf17dd459d
spicytelescope <jbeziaud@gmail.com>
2021-08-08 20:49:53 +0200

    feat(formattedTexts): Adding the 2 formatted texts components and regorganize the index.vue for presentation

---
+++ front/components/atoms/AppError.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="prose-roboto"
+    ><p class="body-2 text-negative-base"><slot></slot></p
+  ></span>
+</template>
+
+<script>
+export default {
+  name: "AppError",
+};
+</script>
+
+<style></style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,16 +1,28 @@
 <template>
-  <AppInputFrame
-    :select="true"
-    :border="false"
-    :options="[{ name: 'test', value: 5 }]"
-  />
+  <div class="flex flex-col items-center p-4 m-4 space-y-4 border">
+    <AppButton variant="contained" size="medium">Master</AppButton>
+    <AppInputFrame
+      :select="true"
+      :border="false"
+      :options="[{ name: 'test', value: 5 }]"
+    />
+    <AppLink link="https://www.youtube.com/user/dirtybiology"
+      >This is a link</AppLink
+    >
+    <AppError>This is an error</AppError>
+  </div>
 </template>
 
 <script>
-import AppInputFrame from "~/components/atoms/AppInputFrame.vue";
+import AppInputFrame from "~/components/atoms/AppInputFrame";
+import AppLink from "~/components/atoms/AppLink";
+import AppError from "~/components/atoms/AppError";
+import AppButton from "~/components/atoms/AppButton.vue";
+
 export default {
-  components: { AppInputFrame },
+  components: { AppInputFrame, AppLink, AppError, AppButton },
 };
 </script>
 
+AppButton
 <style scoped></style>
