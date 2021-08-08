0e3555bb4627d1c4a0f2f4c68ef8bd159053823b
spicytelescope <jbeziaud@gmail.com>
2021-08-08 22:15:37 +0200

    feat(TheHeader, LabeledInputFrame): adding both components made from the first atoms created

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,28 +1,45 @@
 <template>
-  <div class="flex flex-col items-center p-4 m-4 space-y-4 border">
-    <AppButton variant="contained" size="medium">Master</AppButton>
-    <AppInputFrame
+  <div
+    class="flex flex-col items-center p-4 m-4 space-y-4 border prose-montserrat"
+  >
+    <h3>Atoms examples</h3>
+
+    <AppButton variant="contained" size="medium" icon="left"
+      ><template #icon><AppHomeIcon /></template>Master</AppButton
+    >
+    <LabeledInputFrame
+      label="Oui."
       :select="false"
-      :border="false"
+      :border="true"
       :options="[{ name: 'Placeholder', value: 5 }]"
-    />
+      errorMessage="P'tite erreur des familles"
+      ><template #add-on><AppHomeIcon /></template
+    ></LabeledInputFrame>
     <AppLink link="https://www.youtube.com/user/dirtybiology"
       >This is a link</AppLink
     >
     <AppError>This is an error</AppError>
+    <theHeader />
   </div>
 </template>
 
 <script>
-import AppInputFrame from "~/components/atoms/AppInputFrame";
 import AppLink from "~/components/atoms/AppLink";
 import AppError from "~/components/atoms/AppError";
 import AppButton from "~/components/atoms/AppButton.vue";
+import LabeledInputFrame from "~/components/molecules/LabeledInputFrame.vue";
+import AppHomeIcon from "~/components/atoms/icons/AppHomeIcon.vue";
+import TheHeader from "~/components/organisms/TheHeader.vue";
 
 export default {
-  components: { AppInputFrame, AppLink, AppError, AppButton },
+  components: {
+    LabeledInputFrame,
+    AppLink,
+    AppError,
+    AppButton,
+    AppHomeIcon,
+    TheHeader,
+  },
 };
 </script>
-
-AppButton
 <style scoped></style>
