ad5a5355411fc89f4b2cb60ebf01305f6879fd9a
Thomas Cruveilher <sorikairo@gmail.com>
2021-10-06 01:30:11 +0200

    feature(getFlagAfterDate): cast string to date

--- back/flag-service/src/flag/FlagController.ts
+++ back/flag-service/src/flag/FlagController.ts
@@ -14,6 +14,7 @@ import { UserHasNoPixel } from './errors/UserHasNoPixel';
 import { FlagService } from './FlagService';
 import { UserAlreadyOwnAPixelError } from "./errors/UserAlreadyOwnAPixelError";
 import { CooldownTimerHasNotEndedYetError } from "./errors/CooldownTimerHasNotEndedYetError";
+import { parseISO } from 'date-fns';
 
 @Controller('')
 export class FlagController {
@@ -82,8 +83,11 @@ export class FlagController {
 
   @Get('flag/after/:date')
   @Public()
-  async getFlagAfterDate(@Param('date') requestedDate: Date) {
+  async getFlagAfterDate(@Param('date') requestedDate: Date | string) {
     try {
+      if (typeof requestedDate === 'string') {
+        requestedDate = parseISO(requestedDate);
+      }
       const flag = await this.flagService.getFlagAfterDate(requestedDate);
       return flag;
     } catch (e) {
