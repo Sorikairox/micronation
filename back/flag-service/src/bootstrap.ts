e26bf0912a336aec5b6c8362fa1e3dac95674568
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2021-10-09 18:07:11 +0200

    feature(ci): add back ci (#24)
    * feature(ci): add back ci
    Co-authored-by: Thomas Cruveilher <sorikairo@gmail.com>

--- back/flag-service/src/bootstrap.ts
+++ back/flag-service/src/bootstrap.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { v4 } from 'uuid';
 import { JwtExceptionFilter } from './authentication/filters/JwtExceptionFilter';
 import { FlagModule } from './flag/FlagModule';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { FlagService } from './flag/FlagService';
+import { ValidationPipe } from '@nestjs/common';
 import { AuthBackend } from "./user/AuthBackend";
 import { config } from 'dotenv';
 
@@ -17,13 +15,4 @@ export async function bootstrap(appListenPort: string | number = '3000') {
   app.enableCors();
   await app.listen(appListenPort);
   return app;
-}
-
-async function createPixels(app: INestApplication) {
-  const flagService: FlagService = app.get<FlagService>(FlagService);
-  let i = 0;
-  while (i <= 100000) {
-    await flagService.addPixel(v4(), `${"#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})}`);
-    i++;
-  }
 }
