3afea8ee229c257263a90257e0f677111ea89540
Alice Gaudon <alice@gaudon.pro>
2021-10-10 16:32:51 +0200

    fix(front): override vuetify/reset and force vertical scrollbar to auto

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["@/assets/css/fix-html-vertical-scrollbar.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
