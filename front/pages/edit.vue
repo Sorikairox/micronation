0b67901ce93f348e3649e7d871626955b8158d82
Alice Gaudon <alice@gaudon.pro>
2021-10-12 12:26:51 +0200

    feature(front/edit page): prevent view drift when zooming in and out with changing mouse position

--- front/pages/edit.vue
+++ front/pages/edit.vue
@@ -255,12 +255,18 @@ function onWheel(event) {
   const mouseY = event.y - canvas.offsetTop;
   zoomOriginX += mouseX / oldZoomScale - mouseX / zoomScale;
   zoomOriginY += mouseY / oldZoomScale - mouseY / zoomScale;
+  clampCameraPosition();
 
   zoomContext(oldZoomScale, oldZoomOriginX, oldZoomOriginY, zoomScale, zoomOriginX, zoomOriginY);
 
   console.log(zoomScale, zoomOriginX, zoomOriginX);
 
   drawFlag(flagPixelMap);
+}
+
+function clampCameraPosition() {
+  zoomOriginX = Math.min(Math.max(zoomOriginX, 0), canvas.width * (1 - 1 / zoomScale));
+  zoomOriginY = Math.min(Math.max(zoomOriginY, 0), canvas.height * (1 - 1 / zoomScale));
 }
 
 function zoomContext(currentScale, currentOriginX, currentOriginY, newScale, newOriginX, newOriginY, ctx = canvasDrawingContext) {
