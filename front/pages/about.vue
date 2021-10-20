220eae7042542af012ef026d95e5c865c3bf5e4f
Thomas Cruveilher <sorikairo@gmail.com>
2021-10-20 20:59:20 +0200

    feature(about): put everyone info and links
    - closes #32

--- front/pages/about.vue
+++ front/pages/about.vue
@@ -3,20 +3,21 @@
     class="flex flex-col items-center justify-center h-screen text-center  prose-montserrat"
   >
     <div class="max-w-xl space-y-1">
-      <h3>A propos</h3>
+      <h3>Bienvenue au Dirtybiologistan ! </h3>
       <div class="prose-roboto">
         <p class="body-1">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-          dolorem sed saepe aperiam corrupti rerum fugit, consequatur vero
-          veritatis ipsum amet sequi sit quae animi id eos. Hic, error ipsa?
+          En république populaire de DirtyBiology ? Dans le dirty-pays. Comme vous voulez en fait, ce qui compte ici c'est de participer à l'expérience de création du drapeau de cette micronation virtuelle.<br><br><br>
+
+          Le développement de ce concept a été fait durant l'été-automne 2021, et il est le résultat du travail de ces personnes talentueuses :<br><br>
         </p>
+        <ul class="body-1">
+          <li>Thomas (back, front) <a href="//twitter.com/NSorikairo"><img src="https://img.icons8.com/color/25/000000/twitter--v1.png"/></a> <a href="https://www.linkedin.com/in/thomascruveilher/"><img src="https://img.icons8.com/color/25/000000/linkedin.png"/></a></li>
+          <li>Ashpie (back, front) <a href="//ashpie.com/"><img src="https://img.icons8.com/color/25/000000/domain--v1.png"/></a></li>
+          <li>Tif (front) <a href="//www.linkedin.com/in/tifaine-larroque"><img src="https://img.icons8.com/color/25/000000/linkedin.png"/></a></li>
+          <li>Jérémy - Altab (front) <a href="//altab.tech/"><img src="https://img.icons8.com/color/25/000000/domain--v1.png"/></a></li>
+          <li>Jordan Béziaud - Altab (front) <a href="//altab.tech/"><img src="https://img.icons8.com/color/25/000000/domain--v1.png"/></a></li>
+        </ul>
       </div>
-      <h3>Mentions légales</h3>
-      <div class="prose-roboto">
-        <p class="body-1">Léo est vachement beau quand même.</p>
-      </div>
-      <h3>Crédits</h3>
-      <div class="prose-roboto"><p class="body-1">Coucou c’est nous</p></div>
     </div>
   </div>
 </template>
@@ -25,4 +26,14 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+
+a {
+  color: darkred;
+}
+
+img {
+  display: inline-block;
+}
+
+</style>
