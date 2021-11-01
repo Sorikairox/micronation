3f34588797e19ccd805bc65994b1978efa3b1f28
Thomas Cruveilher <sorikairo@gmail.com>
2021-11-01 22:17:26 +0100

    feature(snapshot): create snapshot from previous snapshot

--- back/flag-service/src/flag/snapshot/SnapshotService.ts
+++ back/flag-service/src/flag/snapshot/SnapshotService.ts
@@ -18,6 +18,15 @@ export class FlagSnapshotService {
   }
 
   async createSnapShot(eventId: number) {
+    const lastSnapshot = await this.snapshotRepository.findLast({});
+    if (!lastSnapshot) {
+      const arrayPixel = await this.pixelRepository.getPixelsUntilEventId(eventId);
+      return this.snapshotRepository.createAndReturn({ lastEventId: eventId, pixels: arrayPixel });
+    } else {
+      const arrayPixelNotInSnapshot = await this.pixelRepository.getPixelsBetweenEventIds(lastSnapshot.lastEventId, eventId);
+      const newSnapshotArray = this.mergeArray(lastSnapshot.pixels, arrayPixelNotInSnapshot);
+      return this.snapshotRepository.createAndReturn({ lastEventId: eventId, pixels: newSnapshotArray });
+    }
     const arrayPixel = await this.pixelRepository.getPixelsUntilEventId(eventId);
     return this.snapshotRepository.createAndReturn({ lastEventId: eventId, pixels: arrayPixel });
   }
