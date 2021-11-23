f257376804e5c5a2af59d18948fbe547476dd06f
Thomas Cruveilher <thomas@fyndl.io>
2021-11-23 20:43:48 +0100

    refacto(*): move image generation code into its own method

--- back/flag-service/src/flag/image/ImageService.ts
+++ back/flag-service/src/flag/image/ImageService.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { format, isAfter, set, sub } from 'date-fns';
 import Jimp from 'jimp';
+import { FlagService } from '../FlagService';
 import { GetPixelDto } from '../pixel/dto/GetPixelDto';
 import { mapCoordinatesToTargetRatioRectangleDistribution } from '../utils';
 
 @Injectable()
 export class ImageService {
+
+  constructor(private flagService: FlagService) {
+  }
+
+  async generateFlagImage() {
+    let actualDate = new Date();
+    actualDate = set(actualDate, { year: 2021, date: 21, hours: 12 });
+    while (isAfter(actualDate, new Date(2021, 10, 13))) {
+      const pixels = await this.flagService.getFlagAtDate(actualDate);
+      const image = await this.generateImageFromPixelArray(pixels, 131815, 1/2);
+      const filename = 'flag-' + format(actualDate, 'yyyy-MM-dd') + '.png';
+      await image.writeAsync('./' + filename);
+      console.log('generated ' + filename);
+      actualDate = sub(actualDate, { hours: 24 });
+    }
+  }
 
   generateImageFromPixelArray(pixelArray: Array<GetPixelDto>, flagTotalPixelCount: number, ratio: number, scaleFactor = 8) {
     const localIndexToPixelCoordinateMap = mapCoordinatesToTargetRatioRectangleDistribution(flagTotalPixelCount, ratio);
